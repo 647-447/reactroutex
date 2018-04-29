f7b01c0b9276dab9cdc0dc1417c811cea377394e
Andre Nguyen <ahnguye@ucdavis.edu>
2018-04-28 20:17:53 -0700

    map interface

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
+
           <h1 className="App-title">Status Report</h1>
           <h2 className="Red-text">FIRE. Risk Assessment: High</h2>
-          <h3 className="Red-text">PD: Please evacuate away from the hills.</h3>
+          <h3 className="Red-text">PD: Please evacuate away from park eastbound of Route 84</h3>
           <br></br>
           <button type="button" className="Safe-button">SAFE</button>
           <button type="button" className="Danger-button">DANGER</button>
